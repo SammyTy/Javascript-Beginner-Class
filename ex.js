355085548863c8eb52961951a9e2884bdb5ba796
sam_codex <horlu3091@gmail.com>
2024-02-23 13:40:34 +0100

    fisrt commit

---
+++ ex.js
@@ -0,0 +1,86 @@
+//  Create a variable named result and assign the value true to it using the var keyword.
+var result = true;
+
+//   Declare a variable called userName and initialize it with your name using the let keyword.
+let userName = 'codex7';
+
+// Define a variable named favoriteNumber and assign it a numerical value representing your favorite number.
+let favouriteNumber = 1;
+
+// Declare a variable greeting without assigning any value to it. 
+let greeting;
+// Then, assign the string "Hello, World!" to the variable on the next line
+greeting = 'Hello World';
+
+// Create a constant variable named pi and assign the value 3.14 to it using the const keyword.
+const pi = 3.14;
+
+// Use the typeof operator to check the type of the variable age and assign the result to a variable named typeOfAge.
+let typeOfAge = typeof pi
+console.log( typeOfAge);
+
+let num1=2, num2=4;
+
+let sum = num1 + num2;
+
+let raining = true;
+let isRaining = raining ? 'raning' : 'not rainig' ;
+
+let course = 'javascript Basic'; 
+// course =
+
+let isJavaScriptFun = true;
+
+const city = 'lagos';
+
+const myArray  = [1, 'codex7', true];
+
+// Declare a variable temperature without assigning any value to it.
+let temperature; 
+
+// Later, assign the value 25 to the variable.
+temperature = 25;
+
+// Create a variable named price and assign it a numerical value.
+let price  = 10;
+
+// Then, reassign the value to price by doubling its current value using the const keyword. 
+const doubledPrice = price *2;
+
+// Use the console.log statement to print the message "Hello, JavaScript!" to the console.
+console.log('hello javaScript');
+
+// Declare two variables firstName and lastName and concatenate them to create a variable called fullName. 
+let firstName='sam';
+let lastName='codex';
+let fullName = `${firstName} ${lastName}`;
+
+// Print the value of fullName to the console.
+console.log(`full name:${fullName}`);
+
+// Create a variable named isStudent and assign it a boolean value representing whether you are currently a student or not.
+var isStudent = true;
+if(isStudent){
+    console.log("I am student");
+}else {
+   console.log("I am not a student")  
+};
+
+// Declare a variable carBrand and initialize it with the string "Toyota".
+let carBrand = 'Toyota';
+// Later, update the value to "Honda".
+carBrand = 'Honda';
+
+// Declare a variable named counter using the let keyword and assign it an initial value of 1. Increase the value of the counter by 3
+let counter = 1;
+while (counter <= 3) {
+     // Print the value of counter to the console.
+     console.log(`this is loop number : ${counter}`);
+     counter++;
+}
+
+
+
+
+
+
